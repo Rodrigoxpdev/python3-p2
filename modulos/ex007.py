15d46f6d066b585a6fa300b270ae3f5dce58de9e
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-07-06 16:39:48 -0300

    Create ex007.py

---
+++ modulos/ex007.py
@@ -0,0 +1,43 @@
+#SEPARANDO DIGITOS DE UM NUMERO
+num =  int(input('informe um numero: '))
+
+n = str(num)
+print('ANALISANDO O NUMERO {} '.format(num))
+print('UNIDADE: {} '.format())
+print('DEZENA: {} '.format())
+print('CENTENA: {} '.format())
+print('MILHAR: {} '.format())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
