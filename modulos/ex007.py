c56855100b90a13b19014d66b2ada878a4969b7b
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-07-08 16:05:41 -0300

    #SEPARANDO DIGITOS DE UM NUMERO

--- modulos/ex007.py
+++ modulos/ex007.py
@@ -2,11 +2,17 @@
 num =  int(input('informe um numero: '))
 
 n = str(num)
+
+u = num // 1 % 10  #UNIDADE
+d = num // 10 % 10 #DEZENA
+c = num // 100 % 10 #CENTENA
+m = num // 1000 % 10 #MILHAR
+
 print('ANALISANDO O NUMERO {} '.format(num))
-print('UNIDADE: {} '.format())
-print('DEZENA: {} '.format())
-print('CENTENA: {} '.format())
-print('MILHAR: {} '.format())
+print('UNIDADE: {} '.format(u))
+print('DEZENA: {} '.format(d))
+print('CENTENA: {} '.format(c))
+print('MILHAR: {} '.format(m))
 
 
 
