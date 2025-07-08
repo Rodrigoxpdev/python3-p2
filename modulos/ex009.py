f7c825aaa8f8573b6922b8b84f7a961a916645b7
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-07-08 16:44:31 -0300

    Update ex009.py

--- modulos/ex009.py
+++ modulos/ex009.py
@@ -1,7 +1,9 @@
 #PROCURANDO UMA STRING DENTRO DA OUTRA
 nome = str(input('qual é seu nome completo? ')).strip()
 
-print('seu nome tem silva? {}'.format(nome.))
+#VERIFICANDO SE TEM SILVA NO NOME DIGITADO MESMO SE FOR MAIUSCULO OU MINUSCULO INDEPENDENTE DA POSIÇAO ONDE SE ENCONTRA!
+# 'SILVA' in nome.upper()
+print('seu nome tem silva? {}'.format('Silva' in nome.upper()))
 
 
 
