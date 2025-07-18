1572820ae450157b33c32a611990269b8e254b8d
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-07-18 19:24:54 -0300

    #CONDIÇÕES EM PYTHON

---
+++ condições/ex001.py
@@ -0,0 +1,36 @@
+#CONDIÇÕES EM PYTHON
+tempo = int(input('quantos anos tem seu carro? '))
+
+# if tempo <= 3:
+#     print('seu carro é novo!')
+# else:
+#     print('seu carro está velho!')
+# print('--fim--')
+
+#FORMA SIMPLIFICADA
+# print('carro novo' if tempo<=3 else'carro velho')
+# print('--fim--')
+
+
+#----------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
