b8a8a218a27a08f6b78c5e718d5403a54022bb3a
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-07-18 19:39:34 -0300

    Create ex002.py

---
+++ condições/ex002.py
@@ -0,0 +1,46 @@
+
+nome = str(input('qual seu nome? '))
+nome.lower(nome = 'rodrigo')#COLOCANDO O NOME PARA MINUSCULO
+
+if nome == 'Rodrigo':
+    print('que nome lindo você tem!')
+else:
+    print('seu nome e normal')
+    print('bom dia, {}'.format(nome))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
