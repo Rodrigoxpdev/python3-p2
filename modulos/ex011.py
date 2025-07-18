358ad2a310900192ac76275675146667bc3762f9
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-07-18 18:55:20 -0300

    Update ex011.py

--- modulos/ex011.py
+++ modulos/ex011.py
@@ -1,10 +1,17 @@
+#ANALISE DE NOME
+
+n = str(input('qual seu nome completo? ')).strip()
+nome = n.split() #VAI PEGAR O NOME INTEIRO E FATIAR EM PEDAÇOS 
+#.format(nome[0]) PEGA A POSIÇAO 0 DO NOME
+#.format(nome[len(nome)-1]) LER TODAS AS POSIÇOES DO NOME E PEGA O ULTIMO NOME
+
+#RESULTADO:
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu ultimo nome é {}'.format(nome[len(nome)-1]))
 
 
-
-
-
-
-
+#COM ESSE PROGRAMA PODEMOS ANALISAR NOMES E PEGAR A POSIÇAO DE CADA PARTE DO NOME!
 
 
 
