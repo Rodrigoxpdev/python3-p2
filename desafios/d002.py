092134818062c51fdee1de27f07d22e6423fca48
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-07-18 23:14:21 -0300

    #RADAR ELETRONICO

---
+++ desafios/d002.py
@@ -0,0 +1,42 @@
+#RADAR ELETRONICO
+
+velocidade = float(input('Qual a velocidade atual do carro? '))
+if velocidade > 80:
+    print('MULTADO! você exedeu o limite permitido que é de 80km/h' )
+    multa = (velocidade-80) *7
+    print('você deve pagar uma multa de R${:.2f}!'.format(multa))
+else:
+    print('Tenha um bom dia dirija com segurança!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
