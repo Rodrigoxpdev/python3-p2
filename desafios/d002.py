bffe47c59e7e2ff3d75889847cc97ae7a521c1ab
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-07-18 23:27:57 -0300

    #RADAR ELETRONICO

--- desafios/d002.py
+++ desafios/d002.py
@@ -1,6 +1,10 @@
 #RADAR ELETRONICO
-
+from time import sleep
 velocidade = float(input('Qual a velocidade atual do carro? '))
+print('ANALISANDO...')
+sleep(2)
+
+#CONDIÇAO:
 if velocidade > 80:
     print('MULTADO! você exedeu o limite permitido que é de 80km/h' )
     multa = (velocidade-80) *7
