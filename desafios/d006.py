3c713699225dd302c6d8b9a0872af8f60731c95d
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-07-21 22:38:06 -0300

    #ANO BISSEXTO

---
+++ desafios/d006.py
@@ -0,0 +1,25 @@
+#
+a = int(input('primeiro valor: '))
+b = int(input('segundo valor: '))
+c = int(input('terceiro valor: '))
+
+menor = a 
+
+if b<a and b<c:
+    menor = b
+if c<a and c<b:
+    menor = c
+
+print('O MENOR VALOR FOI {}'.format(menor))
+
+
+
+
+
+
+
+
+
+
+
+
