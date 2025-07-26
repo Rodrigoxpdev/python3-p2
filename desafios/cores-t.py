e51a7f8e3a2ce9d895b48a3dafb1eb24b50c0e0f
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-07-25 22:31:26 -0300

    Create cores-t.py

---
+++ desafios/cores-t.py
@@ -0,0 +1,40 @@
+#STILOS DE CODIGOS 
+# 0 None           
+# 1 Bold           
+# 4 Underline      
+# 7 Negative       
+
+#TEXTO       BACKGROUND
+# 30               40      <BRANCO 
+# 31               41      <VERMELHO
+# 32               42      <VERDE
+# 33               43      <AMARELO
+# 34               44      <AZUL
+# 35               45      <ROXO
+# 36               46      <AZUL CLARO
+# 37               47      <SINZA
+
+#CODIGO PARA MOSTRAR AS CORES
+#\033[0;33;44m
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
